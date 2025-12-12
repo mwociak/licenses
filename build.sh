40831989604d07bc9a9330b0a77b9c85a6d3c401
mwociak <mwociak@gmail.com>
2025-12-12 20:36:42 +0100

    Add verbose build logging

--- build.sh
+++ build.sh
@@ -2,7 +2,18 @@
 # exit on error
 set -o errexit
 
+echo "--- Starting build script ---"
+
+echo "--- Installing packages ---"
 pip install -r requirements.txt
 
+echo "--- Running collectstatic ---"
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+echo "--- Checking for existing migrations ---"
+ls -R myapp/migrations/
+
+echo "--- Running migrate command with verbosity ---"
+python manage.py migrate --verbosity 3
+
+echo "--- Build script finished ---"
